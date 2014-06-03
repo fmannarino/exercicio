688c19eb912f704d623644f98fb0310e67c9a172
fmannarino <mannarino1@hotmail.com>
2014-06-03 15:55:31 -0300

    remoção de espaços em branco desnecessários

--- exercicio.java
+++ exercicio.java
@@ -17,42 +17,35 @@ class Conta{
 	}
 	
 	public void atualiza(double taxa){
-	
 		this.saldo += this.saldo * taxa;
 	}
 }
 
 class ContaCorrente extends Conta{
-
 	public void atualiza(double taxa){
 		this.saldo += this.saldo * taxa * 2;
 	}
 	
 	public void deposita(double valor){
 		this.saldo += valor - 0.10;
-		
 	}
 }
 
 class ContaPoupanca extends Conta{
-
 	public void atualiza(double taxa){
 		this.saldo += this.saldo * taxa * 3;
 	}
-
 }
 
 class exercicio{
-
 	public static void main(String[] args){
-	
 		Conta c = new Conta();
 		Conta cc = new ContaCorrente();
 		Conta cp = new ContaPoupanca();
 		c.deposita(1000);
 		cc.deposita(1000);
 		cp.deposita(1000);
-		c.atualiza(0.01);
+		c.atualiza(0.02);
 		cc.atualiza(0.01);
 		cp.atualiza(0.01);
 		System.out.println(c.getSaldo());
@@ -60,9 +53,3 @@ class exercicio{
 		System.out.println(cp.getSaldo());
 	}
 }
-
-
-
-
-
-
